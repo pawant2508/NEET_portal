45a30bf9a89d328454de0349bd7da32c059a3287
pawant2508 <129742506+pawant2508@users.noreply.github.com>
2024-04-09 22:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import streamlit as st # type: ignore
-from streamlit_option_menu import option_menu  # type: ignore
+import streamlit as st
+from streamlit_option_menu import option_menu
 import home, account, mock, mock_test_1, mock_test_2, mock_test_3, mock_test_4, mock_test_5, mock_test_6, mock_test_7, mock_test_8, mock_test_9, mock_test_10, prediction
 
 st.set_page_config(
@@ -17,50 +17,40 @@ class MultiApp:
             "function": func
         })
 
-    def run(self):  # Add self parameter here
-        with st.sidebar:        
-            app = option_menu(
-                menu_title='NEET Qualifying Portal',
-                options=['Home', 'Account', 'Mock', 'Mock_Test_1', 'Mock_Test_2', 'Mock_Test_3', 'Mock_Test_4', 'Mock_Test_5', 'Mock_Test_6', 'Mock_Test_7', 'Mock_Test_8', 'Mock_Test_9', 'Mock_Test_10', 'Prediction'],
-                icons=['house-fill', 'person-circle','star-fill', 'star', 'star', 'star', 'star', 'star', 'star', 'star', 'star', 'star','star', 'info-circle'],  # Adjusted the icons list
-                menu_icon='chat-text-fill',
-                default_index=1,
-                styles={
-                    "container": {"padding": "5!important", "background-color": 'black'},
-                    "icon": {"color": "white", "font-size": "23px"}, 
-                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
-                    "nav-link-selected": {"background-color": "#02ab21"},
-                }
-            )
+    def option_menu(self):  # Corrected the method signature
+        menu_title = 'NEET Qualifying Portal'
+        options = ['Home', 'Account', 'Mock', 'Mock_Test_1', 'Mock_Test_2', 'Mock_Test_3', 'Mock_Test_4', 'Mock_Test_5', 'Mock_Test_6', 'Mock_Test_7', 'Mock_Test_8', 'Mock_Test_9', 'Mock_Test_10', 'Prediction']
+        selected_option = st.sidebar.selectbox(menu_title, options)
 
-        if app == "Home":
+        # Call the corresponding app function based on the selected option
+        if selected_option == 'Home':
             home.app()
-        if app == "Account":
+        elif selected_option == 'Account':
             account.app()
-        if app == "Mock":
+        elif selected_option == 'Mock':
             mock.app()
-        if app == "Mock_Test_1":
+        elif selected_option == 'Mock_Test_1':
             mock_test_1.app()
-        if app == "Mock_Test_2":
+        elif selected_option == 'Mock_Test_2':
             mock_test_2.app()
-        if app == "Mock_Test_3":
+        elif selected_option == 'Mock_Test_3':
             mock_test_3.app()
-        if app == "Mock_Test_4":
+        elif selected_option == 'Mock_Test_4':
             mock_test_4.app()
-        if app == "Mock_Test_5":
+        elif selected_option == 'Mock_Test_5':
             mock_test_5.app()
-        if app == "Mock_Test_6":
+        elif selected_option == 'Mock_Test_6':
             mock_test_6.app()
-        if app == "Mock_Test_7":
+        elif selected_option == 'Mock_Test_7':
             mock_test_7.app()
-        if app == "Mock_Test_8":
+        elif selected_option == 'Mock_Test_8':
             mock_test_8.app()
-        if app == "Mock_Test_9":
+        elif selected_option == 'Mock_Test_9':
             mock_test_9.app()
-        if app == "Mock_Test_10":
-            mock_test_10.app()    
-        if app == "Prediction":
-            prediction.app()        
+        elif selected_option == 'Mock_Test_10':
+            mock_test_10.app()
+        elif selected_option == 'Prediction':
+            prediction.app()
 
-multi_app = MultiApp()  # Create an instance of MultiApp
-multi_app.run()  # Call the run method of MultiApp instance
+multi_app = MultiApp()
+multi_app.option_menu()
